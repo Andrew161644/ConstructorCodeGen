bd4d8e2381a3cd1ee3e9d0846c90269f72722330
Andrew161644 <truba24032016@mail.ru>
2023-09-05 09:22:31 +0500

    Исправить шаблон IMPORTS

--- templateConstants.ts
+++ templateConstants.ts
@@ -1,4 +1,4 @@
-export const IMPORTS = "?%IMPORTS%?";
+export const IMPORTS = "?%IMPORTS?%";
 export const COMPONENT_NAME = "?%COMPONENT_NAME?%";
 export const COMPONENT_TYPE = "?%COMPONENT_TYPE?%";
 export const PATH = "?%PATH?%";
