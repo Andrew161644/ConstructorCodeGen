84530d667b4cd71fa855f866cd6b984458a009a2
Andrew161644 <truba24032016@mail.ru>
2023-09-10 16:09:56 +0500

    Исправить баг с пропсами

--- modules/buildProps/buildProps.ts
+++ modules/buildProps/buildProps.ts
@@ -1,8 +1,12 @@
 import { TreeItem } from "../../coreTypes";
+import { isGroupElement } from "../../utils";
 
 import { buildPropsDict } from "./buildPropsDict";
 
 export const buildFormElementProps = (item: TreeItem, tabs: string) => {
+  if (isGroupElement(item)) {
+    return "";
+  }
   return buildPropsDict[item.type](item, tabs);
 };
 
